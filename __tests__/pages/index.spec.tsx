ccf9884728d0cd24a4cf49fffd40bc48447ef28b
Akshay.Thool <akshay.thool@ibm.com>
2021-05-22 18:05:57 +0530

    Adding Github testing support

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -3,6 +3,6 @@ import Index from 'pages/index';
 
 test('renders index page', () => {
   const { getByText } = render(<Index />);
-  const helloWorldElement = getByText('Hello Worldw');
+  const helloWorldElement = getByText('Hello World');
   expect(helloWorldElement).toBeInTheDocument();
 });
